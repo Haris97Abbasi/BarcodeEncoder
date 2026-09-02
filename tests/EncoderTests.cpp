0e1f1bb33dc55bb1e84c51849b68a9d70b15c98a
Haris Abbasi <haris97duisburg@gmail.com>
2026-09-02 07:10:28 +0500

    Unit  Tests

--- tests/EncoderTests.cpp
+++ tests/EncoderTests.cpp
@@ -10,3 +10,34 @@ TEST(EncodeTest, EncodesSampleInput) {
     Encoder encoder;
     EXPECT_EQ(encoder.encode("HELLO123"), (std::vector<int>{104, 40, 37, 44, 44, 47, 17, 18, 19, 8, 106}));
 }
+
+TEST(EncodeTest, EncodesSingleCharacter) {
+    Encoder encoder;
+    EXPECT_EQ(encoder.encode("A"), (std::vector<int>{104, 33, 34, 106}));
+}
+
+TEST(EncodeTest, EncodesMessageWithSpace) {
+    Encoder encoder;
+    EXPECT_EQ(encoder.encode("A B"), (std::vector<int>{104, 33, 0, 34, 33, 106}));
+}
+
+TEST(EncodeTest, EncodesMessageWithSpecialCharacter) {
+    Encoder encoder;
+    EXPECT_EQ(encoder.encode("A.B"), (std::vector<int>{104, 33, 14, 34, 61, 106}));
+}
+
+TEST(EncodeTest, ThrowsOnEmptyString) {
+    Encoder encoder;
+    EXPECT_THROW(encoder.encode(""), std::invalid_argument);
+}
+
+TEST(EncodeTest, ThrowsOnInvalidCharacter) {
+    Encoder encoder;
+    EXPECT_THROW(encoder.encode("Schlüssel"), std::invalid_argument);
+    EXPECT_THROW(encoder.encode("Bär"), std::invalid_argument);
+}
+
+TEST(ChecksumTest, CalculatesChecksum) {
+    Encoder encoder;
+    EXPECT_EQ(encoder.calculateChecksum({40, 37, 44, 44, 47, 17, 18, 19}), 8);
+}
